a4ce158d1e5dcdf1434689389caee58fe4fb20e0
eogj1 <123607061+eogj1@users.noreply.github.com>
2023-10-04 20:49:23 -0400

    move code in server.js

--- server.js
+++ server.js
@@ -9,25 +9,24 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-      console.log(res); //true
-    });
-  });
+
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-  var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
-
-  var result = bcrypt.compareSync(myPlaintextPassword, hash);
-    console.log(result);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res);
+    });
+  });
 
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
