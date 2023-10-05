fcc7f91d386e38ebc904e3a8003f6524a0498a51
eogj1 <123607061+eogj1@users.noreply.github.com>
2023-10-04 20:45:02 -0400

    add semicolons in server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-  bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
@@ -19,11 +17,12 @@ const someOtherPlaintextPassword = 'pass123';
     });
   });
 
-  var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+  var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
     console.log(hash);
-  var result = bcrypt.compareSync(myPlaintextPassword, hash)
-    console.log(result);
 
+  var result = bcrypt.compareSync(myPlaintextPassword, hash);
+    console.log(result);
 
 //END_ASYNC
 
